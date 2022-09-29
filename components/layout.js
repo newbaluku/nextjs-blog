2508f76e71a0d63bc693cdd4f5c4f4dbfc23d3ab
newbaluku <newbaluku@gmail.com>
2022-09-29 20:52:52 +0800

    Update layout.js

--- components/layout.js
+++ components/layout.js
@@ -4,8 +4,8 @@ import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
-const name = 'LP';
-export const siteTitle = 'Next.js Sample Website';
+const name = 'PK';
+export const siteTitle = 'PK Next.js Sample Website';
 
 export default function Layout({ children, home }) {
   return (
